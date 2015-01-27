86a3d43d15dbf190876fefc12aa7a7fb10d31ba5
Sinyeol An <soundlake@gmail.com>
2015-01-27 11:02:07 +0100

    solve step1

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,31 @@
-# 0. Download the dataset
+# 0. Define some utilities
+mload <- function(filename) {
+    fp <- file(filename, 'r')
+    df <- read.table(fp)
+    close(fp)
+    df
+}
+
 # 1. Merges the training and the test sets to create one data set.
+mstep1 <- function() {
+    # load names for the dataset
+    nameset <- mload('UCI HAR Dataset/features.txt')
+    nameset <- as.character(nameset[[2]])
+    
+    # load training data
+    trainset <- mload('UCI HAR Dataset/train/X_train.txt')
+    
+    # load test data
+    testset <- mload('UCI HAR Dataset/test/X_test.txt')
+    
+    # set names on two dataset
+    names(trainset) <- nameset
+    names(testset) <- nameset
+    
+    rbind(trainset, testset)
+}
+mdata <- mstep1()
+
 # 2. Extracts only the measurements on the mean and standard deviation
 #    for each measurement.
 # 3. Uses descriptive activity names to name the activities in the data set.
