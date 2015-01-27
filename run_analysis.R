9469bf12d05a601f08ee2f1306bf1682a383a53e
Sinyeol An <soundlake@gmail.com>
2015-01-27 11:08:00 +0100

    solve step2

--- run_analysis.R
+++ run_analysis.R
@@ -22,6 +22,11 @@ mstep1 <- function() {
 mdata <- mstep1()
 # 2. Extracts only the measurements on the mean and standard deviation
 #    for each measurement.
+mstep2 <- function(mdata) {
+    index <- grep('mean|std', names(mdata))
+    mdata[,index]
+}
+mdata <- mstep2(mdata)
 # 3. Uses descriptive activity names to name the activities in the data set.
 # 4. Appropriately labels the data set with descriptive variable names.
 # 5. From the data set in step 4, creates a second, independent tidy data set
